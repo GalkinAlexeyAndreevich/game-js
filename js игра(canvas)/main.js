c1c5e00aa5e2dcf7a838bd7c31157a6765f8bd45
Galkin Alexey Andreevich <97546730+GalkinAlexeyAndreevich@users.noreply.github.com>
2023-04-20 13:22:04 +0300

    Коллизия сверху

--- js игра(canvas)/main.js
+++ js игра(canvas)/main.js
@@ -35,7 +35,7 @@ class Player1 {
         this.updateLocation()
     }
     updateLocation() {
-        console.log(this.x, this.y);
+        // console.log(this.x, this.y);
         if (this.y > this.maxY) this.y = this.maxY
         if (this.y < this.minY) this.y = this.minY
         ctx.fillStyle = this.colorObj
@@ -51,36 +51,55 @@ class Player1 {
 
 class Ball {
     constructor() {
-        this.width = 20,
-        this.height = 20,
+        this.radius = 20,
         this.x = 330,
-        this.y = 350,
+        this.y = 400,
         this.maxX = canvas.width,
         this.maxY = canvas.height,
         this.minX = 0,
         this.minY = 0,
         this.colorObj = "orange",
-        this.velocity = -7
+        this.velocity = -5
+        this.direction = 0
+        this.max = 2
+        this.min = -5
         this.element = document.querySelector("#ball")
     }
     updateLocation() {
+
+        console.log(this.y);
         if (this.x > this.maxX) this.x = this.maxX
         if (this.x < this.minX) this.x = this.minX
         if (this.y > this.maxY) this.y = this.maxY
         if (this.y < this.minY) this.y = this.minY
+        
+        ctx.beginPath();
         ctx.fillStyle = this.colorObj
-        ctx.fillRect(this.x,this.y,this.width,this.height )
+        ctx.arc(this.x,this.y-this.radius,this.radius,0,Math.PI *2)
+        ctx.fill()
+
+        // ctx.stroke()
+        // ctx.fillRect(this.x,this.y,this.radius,this.radius )
     }
     moveBall() {
+        // this.direction = Math.floor(Math.random() * (this.radius + this.radius) -this.radius)
         this.x = this.x + this.velocity
+        this.y = this.y + this.direction
         this.updateLocation()
     }
     changeDirection() {
-        this.velocity = -this.velocity
+        if(this.direction ==0){
+           this.direction = Math.floor(Math.random() * (this.max - this.min) + this.min) 
+        }
+        else{
+            this.direction = -this.direction
+        }
+        // this.velocity = -this.velocity
     }
     beginPosition() {
-        this.x = 390
-        this.y = 300
+        this.x = 500
+        this.y = 400
+        this.direction = 0
         this.updateLocation()
     }
 }
@@ -106,9 +125,11 @@ let player2 = new Player1(PlayerInfo2)
 let ball = new Ball()
 
 function collision(player, ball) {
-    console.log(player.y + player.height, ball.y);
-    if (player.y <= ball.y + 10 && player.y + player.height - 5 >= ball.y && player.x - player.width <= ball.x && player.x >= ball.x - ball.width) {
-        console.log("collide");
+    if (player.y <= ball.y && player.y + player.height   >= ball.y- ball.radius *2 && player.x - player.width <= ball.x && player.x >= ball.x - ball.radius *2) {
+        ball.velocity = -ball.velocity
+        return true
+    }
+    if(ball.y - ball.radius *2<=0 ){
         return true
     }
     return false
@@ -116,8 +137,6 @@ function collision(player, ball) {
 }
 
 const start = () => {
-
-
 
     player1.beginPosition()
     player2.beginPosition()
@@ -144,11 +163,10 @@ function gameScore() {
 }
 const update = () => {
     window.requestAnimationFrame(update)
-    ctx.fillStyle = "rgba(255, 255, 255, 0.3)"
+    ctx.fillStyle = "rgba(255, 255, 255, 1)"
     ctx.fillRect(0,0,canvas.width,canvas.height)
     player1.updateLocation()
     player2.updateLocation()
-    // ball.updateLocation()
     if (ball.x <= 0) {
         player2.score += 1
         gameScore()
@@ -159,6 +177,7 @@ const update = () => {
     }
     ball.moveBall()
     if (collision(player1, ball) || collision(player2, ball)) {
+        console.log("collide");
         ball.changeDirection()
 
     }
