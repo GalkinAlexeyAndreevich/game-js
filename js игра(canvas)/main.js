cb36690ee9960ac6255e68057d4e196ad6f7eb51
Galkin Alexey Andreevich <97546730+GalkinAlexeyAndreevich@users.noreply.github.com>
2023-04-27 09:31:27 +0300

    Change style

--- js игра(canvas)/main.js
+++ js игра(canvas)/main.js
@@ -64,7 +64,7 @@ class Ball {
         this.maxY = canvas.height,
         this.minX = 0,
         this.minY = 30,
-        this.colorObj = "orange",
+        this.colorObj = "white",
         this.velocity = -10
         this.direction = 0
         this.isNegative = false
@@ -94,7 +94,7 @@ class Ball {
     }
     changeDirection() {
         if(this.direction == 0){
-            this.direction = 3
+            this.direction = 5
         }
         this.isNegative = !this.isNegative
         if(this.isNegative){
@@ -117,7 +117,7 @@ class Ball {
 const PlayerInfo1 = {
     x:10,
     y:300,
-    colorObj: "red",
+    colorObj: "white",
     up:"KeyW",
     down:"KeyS"
     
@@ -125,7 +125,7 @@ const PlayerInfo1 = {
 const PlayerInfo2 = {
     x:canvas.width-30,
     y:300,
-    colorObj: "red",
+    colorObj: "white",
     up:"ArrowUp",
     down:"ArrowDown"
 
@@ -137,8 +137,8 @@ let ball = new Ball()
 
 function collision(player, ball) {
     // если мяч и игрок соприкоснулись
-    if (player.y <= ball.y && player.y + player.height >= ball.y- ball.radius *2 &&
-        player.x - player.width <= ball.x && player.x >= ball.x - ball.radius *2) {
+    if (player.y < ball.y && player.y + player.height > ball.y- ball.radius *2 &&
+        player.x - player.width < ball.x && player.x > ball.x - ball.radius *2) {
         if(ball.velocity >=0){
             ball.velocity = ball.velocity + ball.coef/5
         }
@@ -223,6 +223,18 @@ function gameScore() {
 
 }
 
+const clearCanvas = ()=>{
+    ctx.fillStyle = "black"
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+    ctx.setLineDash([64, 18]);
+    ctx.beginPath(); 
+    ctx.lineWidth = 5
+    ctx.moveTo(canvas.width/2, 0);   
+    ctx.lineTo(canvas.width/2, canvas.height);  
+    ctx.strokeStyle = 'white';
+    ctx.stroke();
+}
+
 const update = () => {
     // бесконечный цикл обновления объектов и их коллизия
     window.requestAnimationFrame(update)
@@ -231,12 +243,7 @@ const update = () => {
         return
     }
     // Очищаем canvas
-    ctx.fillStyle = "rgba(255, 255, 255, 1)"
-    ctx.fillRect(0,0,canvas.width,canvas.height)
-    ctx.beginPath(); 
-    ctx.moveTo(canvas.width/2, 0);   
-    ctx.lineTo(canvas.width/2, canvas.height);  
-    ctx.stroke();  
+    clearCanvas()
     // Размещаем игроков и мяч
     player1.updateLocation()
     player2.updateLocation()
@@ -256,7 +263,6 @@ const update = () => {
         ball.coef = ball.coef + 1
         ball.changeDirection()
     }
-
 }
 
 start()
